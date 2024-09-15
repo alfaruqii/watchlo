eca166cec7483cf642b0355f2c16b346e0bf77b9
alfaruqii <bumbblechover@gmail.com>
2024-09-16 01:36:48 +0700

    feat(component): add state management for theme(zustand)

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,9 +2,11 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { ToggleTheme } from "./ToggleTheme";
+import { useThemeStore } from "@/app/store/themeStore";
 
 export const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const { theme } = useThemeStore();
 
   // Add event listener for scroll event
   useEffect(() => {
@@ -25,17 +27,17 @@ export const Navbar = () => {
   return (
     <>
       <div
-        className={`sticky top-0 z-50 flex items-center justify-between py-2 px-4 shadow sm:px-12 transition-all duration-300 ${isScrolled ? "bg-base-100/80 backdrop-blur-lg" : "bg-base-100"
-          }`}
+        className={`${isScrolled ? theme === "garden" ? "bg-base-100/80 backdrop-blur-lg" : "bg-black/80 backdrop-blur-lg" : "bg-base-100"} sticky top-0 z-50 flex items-center justify-between px-4 py-2 drop-shadow-lg transition-all duration-300
+          sm:px-12`}
       >
-        <div className="drop-shadow-lg">
+        <div className={`drop-shadow-lg ${theme === "garden" ? "text-black" : "text-white"}`}>
           <Link href="/">
             <span className="sm:text-lg ">Watch</span>
             <span className="font-magnatbold sm:text-lg">milo</span>
           </Link>
         </div>
         <div className="flex items-center gap-2">
-          <button className="btn btn-sm rounded drop-shadow border-2 border-white">
+          <button className={`${theme === "garden" ? "border-gray-800" : "border-gray-300"} btn btn-sm rounded border-2 drop-shadow`}>
             <svg
               stroke="currentColor"
               fill="currentColor"
