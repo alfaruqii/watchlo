d82651be3e727958e6c30a0cd49befc55bfdceaf
alfaruqii <bumbblechover@gmail.com>
2024-09-16 19:15:35 +0700

    change(services): split anime services

--- app/services/index.ts
+++ app/services/index.ts
@@ -3,19 +3,31 @@ import { API_V1, API_V2 } from "../lib/api"
 import { AxiosResponse } from 'axios';
 
 
-const DashboardAnimeService = {
-  getTrendingAnime: async (): Promise<AxiosResponse> => {
-    return API_V2().get(`/trending`);
-  },
+const AnimeServiceV1 = {
   getRecentEpisode: async (): Promise<AxiosResponse> => {
     return API_V1().get(`/anime/gogoanime/recent-episodes`);
   },
+  getAnimeStream: async (id: string): Promise<AxiosResponse> => {
+    return API_V1().get(`/anime/gogoanime/watch/${id}`);
+  },
+  getAnimeInfoV1: async (id: string): Promise<AxiosResponse> => {
+    return API_V1().get(`/anime/gogoanime/info/${id}`);
+  },
+}
+
+const AnimeServiceV2 = {
+  getTrendingAnime: async (): Promise<AxiosResponse> => {
+    return API_V2().get(`/trending`);
+  },
   getPopularAnime: async (): Promise<AxiosResponse> => {
     return API_V2().get(`/popular`);
+  },
+  getAnimeInfoV2: async (id: string): Promise<AxiosResponse> => {
+    return API_V2().get(`/info/${id}`);
   },
   getRecommendationAnime: async (params: AnimeRecQueryParams): Promise<AxiosResponse> => {
     return API_V2().get(`/recommendations/${params.id}`);
   },
 }
 
-export default DashboardAnimeService;
+export { AnimeServiceV1, AnimeServiceV2 };
