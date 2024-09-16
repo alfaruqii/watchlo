fa12c618ae46523c59edaa8eba629210cb208c74
alfaruqii <bumbblechover@gmail.com>
2024-09-16 19:14:15 +0700

    feat(component): update navbar default theme

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { ToggleTheme } from "./ToggleTheme";
 import { useThemeStore } from "@/app/store/themeStore";
+import ToggleSearch from "./ToggleSearch";
 
 export const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -30,26 +31,25 @@ export const Navbar = () => {
         className={`${isScrolled ? theme === "garden" ? "bg-base-100/80 backdrop-blur-lg" : "bg-black/80 backdrop-blur-lg" : "bg-base-100"} sticky top-0 z-50 flex items-center justify-between px-4 py-2 drop-shadow-lg transition-all duration-300
           sm:px-12`}
       >
-        <div className={`drop-shadow-lg ${theme === "garden" ? "text-black" : "text-white"}`}>
+        <div className={`drop-shadow-lg ${theme === "black" ? "text-white" : "text-black"}`}>
           <Link href="/">
             <span className="sm:text-lg ">Watch</span>
             <span className="font-magnatbold sm:text-lg">milo</span>
           </Link>
         </div>
+        <div className={`drop-shadow-lg hidden sm:flex gap-10 font-normal ${theme === "black" ? "text-white" : "text-black"}`}>
+          <Link href="/">
+            <span className="sm:text-lg">All</span>
+          </Link>
+          <Link href="/anime">
+            <span className="sm:text-lg">Anime</span>
+          </Link>
+          <Link href="/manga">
+            <span className="sm:text-lg ">Manga</span>
+          </Link>
+        </div>
         <div className="flex items-center gap-2">
-          <button className={`${theme === "garden" ? "border-gray-800" : "border-gray-300"} btn btn-sm rounded border-2 drop-shadow`}>
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 512 512"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
-            </svg>
-          </button>
+          <ToggleSearch />
           <ToggleTheme />
         </div>
       </div>
