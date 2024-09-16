b7adc3b31ea273b32d0df2b381bb0df5110f3211
alfaruqii <bumbblechover@gmail.com>
2024-09-16 19:13:40 +0700

    feat(component): add key to each Suspense[AnimeContainerCard]

--- app/components/card/animecard/AnimeContainerCard.tsx
+++ app/components/card/animecard/AnimeContainerCard.tsx
@@ -7,21 +7,20 @@ import { AnimeType } from '@/app/types/anime.type'
 interface AnimeContainerProps {
   animes: AnimeType[]
   containerTitle: string
-  isTrending?: boolean // A flag to differentiate between trending/popular, etc.
-  isPopular?: boolean
 }
 
 export const AnimeContainerCard = ({ animes, containerTitle }: AnimeContainerProps) => {
   const { theme } = useThemeStore();
   return (
-    <div className="p-4">
+    <div className="p-4 overflow-hidden">
       <p className={`mb-4 pb-2 font-bold border-b ${theme === "garden" ? "border-black" : "border-gray-200"}`}>{containerTitle}</p>
-      <div className="flex embla__container gap-4 relative w-full overflow-x-scroll no-scrollbar">
+      <div className="flex embla__container gap-4 relative w-full overflow-x-scroll scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-gray-800 scrollbar-track-gray-300 pb-2">
         {
           animes.map((anime) => (
-            <Suspense fallback={<div className="card max-h-44 min-h-44 min-w-32 rounded sm:max-h-72 sm:min-h-72 sm:min-w-52">Loading feed...</div>}>
+            <Suspense
+              key={anime.id}
+              fallback={<div className="card max-h-44 min-h-44 min-w-32 rounded sm:max-h-72 sm:min-h-72 sm:min-w-52">Loading feed...</div>}>
               <AnimeCard
-                key={anime.id}
                 anime={anime}
               />
             </Suspense>
