5ae797a4d2d9e0cdaca1e43492fadee96f2a2eb6
alfaruqii <bumbblechover@gmail.com>
2024-09-18 02:38:39 +0700

    feat(component)[Quality]: initialize Quality component

---
+++ app/components/quality/Quality.tsx
@@ -0,0 +1,23 @@
+import { Source } from '../../types/anime.type'
+import React from 'react'
+
+function Quality({ sources, handleQualityChange }: { sources: Source[]; handleQualityChange: (url: string) => void }) {
+  return (
+    <>
+      <div className="dropdown dropdown-bottom sm:dropdown-right">
+        <div tabIndex={0} role="button" className="btn m-1">Quality video</div>
+        <div tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-full p-2 shadow">
+          {
+            sources.map((source) => (
+              <li key={source.quality} onClick={() => handleQualityChange(source.url)}>
+                <a>{source.quality}</a>
+              </li>
+            ))
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Quality
