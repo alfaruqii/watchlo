83720088c195a9caa1d6188e279c42da09d7362b
alfaruqii <bumbblechover@gmail.com>
2024-09-18 02:35:22 +0700

    feat(page): update page anime

--- app/anime/page.tsx
+++ app/anime/page.tsx
@@ -1,16 +1,15 @@
 import { AnimeContainerCard } from "../components/card/animecard/AnimeContainerCard";
 import HeroAnimeCarousel from "../components/hero/anime/HeroAnimeCarousel";
-import DashboardAnimeService from "../services";
+import { AnimeServiceV1, AnimeServiceV2 } from "../services";
 
 export default async function Home() {
-  const { data: dataTrending } = await DashboardAnimeService.getTrendingAnime();
-  const { data: dataRecent } = await DashboardAnimeService.getRecentEpisode();
-  const { data: dataPopular } = await DashboardAnimeService.getPopularAnime();
-  console.log(dataRecent)
+  const { data: dataTrending } = await AnimeServiceV2.getTrendingAnime();
+  const { data: dataRecent } = await AnimeServiceV1.getRecentEpisode();
+  const { data: dataPopular } = await AnimeServiceV2.getPopularAnime();
 
   return (
     <>
-      <div className="flex flex-col gap-4 min-h-screen">
+      <div className="flex flex-col gap-4 min-h-fit">
         <HeroAnimeCarousel animes={dataTrending.results} />
         <AnimeContainerCard containerTitle="Recently Updated 🎬" animes={dataRecent.results} />
         <AnimeContainerCard containerTitle="Most Popular 💯" animes={dataPopular.results} />
