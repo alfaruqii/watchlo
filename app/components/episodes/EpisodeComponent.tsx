2aecea80b416793ba2e0f9be6c971a042eebaa48
alfaruqii <bumbblechover@gmail.com>
2024-09-18 02:36:01 +0700

    feat(component): update EpisodeComponent

--- app/components/episodes/EpisodeComponent.tsx
+++ app/components/episodes/EpisodeComponent.tsx
@@ -1,21 +1,28 @@
-import { AnimeDetails } from "@/app/types/anime.type"
+"use client";
+import { useThemeStore } from "@/app/store/themeStore";
 import Link from "next/link"
 
 type Episode = {
   id: string;
-  episode: string;
+  epQuery: string;
+  titleAndEpisode: string;
   episodeNumber: number;
 }
 
-function EpisodeComponent({ id, episode, episodeNumber }: Episode) {
-  const route = { pathname: `/anime/watch/${id}`, query: { animeId: episode, ep: episodeNumber } }
+function EpisodeComponent({ id, titleAndEpisode, episodeNumber, epQuery }: Episode) {
+  const { theme } = useThemeStore();
+  const route = { pathname: `/anime/watch/${id}`, query: { animeTitle: titleAndEpisode, ep: episodeNumber } }
+
+  const conditionEpisode = String(episodeNumber) === epQuery;
   return (
     <>
       <Link href={route}>
-        <div className="btn">
-          {
-            episode
-          }
+        <div className={`btn min-w-16 2xl:min-w-24 ${conditionEpisode ? `${theme === "garden" ? "bg-gray-700 text-white" : "bg-gray-600"}` : ""}`}>
+          <p>
+            {
+              episodeNumber
+            }
+          </p>
         </div>
       </Link>
     </>
