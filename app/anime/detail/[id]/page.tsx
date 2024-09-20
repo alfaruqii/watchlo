0bc7fdee232115908f1faa5aa73316092c31ce34
alfaruqii <bumbblechover@gmail.com>
2024-09-20 21:44:01 +0700

    feat(page): update specific detail page

--- app/anime/detail/[id]/page.tsx
+++ app/anime/detail/[id]/page.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { Suspense } from 'react';
 
-function DetailPage() {
+import { Banner } from '@/components/detail/banner/Banner';
+import CardBanner from '@/components/detail/cardbanner/CardBanner';
+import EpisodesContainer from '@/components/detail/episodes/EpisodesContainer';
+import InfoDetails from '@/components/detail/infodetails/InfoDetails';
+import Trailer from '@/components/media/Trailer';
+import SkeletonEpisodes from '@/components/skeleton/SkeletonEpisodes';
+
+import { AnimeServiceV2 } from '@/services';
+import { AnimeInfo } from '@/types/anime.type';
+
+async function DetailPage({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const { data: dataInfo }: { data: AnimeInfo } = await AnimeServiceV2.getAnimeInfoV2(id);
+
   return (
-    <div>DetailPage</div>
+    <>
+      <div>
+        <Banner {...dataInfo} />
+        <div className="p-6">
+          <CardBanner {...dataInfo} />
+          <InfoDetails {...dataInfo} />
+          <Trailer trailer={dataInfo.trailer} />
+          <Suspense
+            fallback={<SkeletonEpisodes />}>
+            {
+              dataInfo.id_provider?.idGogo &&
+              <EpisodesContainer {...dataInfo} />
+            }
+          </Suspense>
+        </div>
+      </div>
+    </>
   )
 }
 
