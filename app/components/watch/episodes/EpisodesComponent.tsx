8d0afd5dd600c3679d4b7d2cf6b9b4ee57ee2720
alfaruqii <bumbblechover@gmail.com>
2024-09-20 21:56:20 +0700

    feat(component)[EpisodesComponent]: create EpisodesComponent for watch page

---
+++ app/components/watch/episodes/EpisodesComponent.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { AnimeDetails, Episode } from "@/types/anime.type"
+import EpisodeComponent from "./EpisodeComponent"
+
+interface Anime extends AnimeDetails {
+  ep: string
+}
+
+function EpisodesComponent(anime: Anime) {
+  return (
+    <>
+      <div className="lg:max-w-[50%] flex flex-col pt-1.5">
+        <p className="pb-1">
+          Now Playing Episode {anime.ep}
+        </p>
+        <div className="flex flex-wrap gap-2 scrollbar scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-gray-800 scrollbar-track-gray-300 max-h-96 2xl:max-h-[30rem] overflow-y-scroll ">
+          {
+            anime.episodes.map((i: Episode) => <EpisodeComponent key={i.id} id={i.id} titleAndEpisode={anime.id as string} episodeNumber={i.number} epQuery={anime.ep} />)
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default EpisodesComponent;
