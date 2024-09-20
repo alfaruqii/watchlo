8156ca0f3f908f47e75d5f45bdc21915178a5337
alfaruqii <bumbblechover@gmail.com>
2024-09-20 21:55:49 +0700

    feat(component)[EpisodeComponent]: create Episode component for watch page

---
+++ app/components/watch/episodes/EpisodeComponent.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useThemeStore } from "@/store/themeStore";
+import Link from "next/link"
+
+type Episode = {
+  id: string;
+  epQuery: string;
+  titleAndEpisode: string;
+  episodeNumber: number;
+}
+
+function EpisodeComponent({ id, titleAndEpisode, episodeNumber, epQuery }: Episode) {
+  const { theme } = useThemeStore();
+  const route = { pathname: `/anime/watch/${id}`, query: { animeTitle: titleAndEpisode, ep: episodeNumber } }
+
+  const conditionEpisode = String(episodeNumber) === epQuery;
+  return (
+    <>
+      <Link href={route}>
+        <div className={`btn min-w-24 2xl:min-w-24 ${conditionEpisode ? `${theme === "garden" ? "bg-gray-700 text-white" : "bg-gray-600"}` : ""}`}>
+          <p>
+            {
+              episodeNumber
+            }
+          </p>
+        </div>
+      </Link>
+    </>
+  )
+}
+
+export default EpisodeComponent;
