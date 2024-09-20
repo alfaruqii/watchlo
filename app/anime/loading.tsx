0712939040d8a36fc9b7ca2102ad4a0103059358
alfaruqii <bumbblechover@gmail.com>
2024-09-20 21:45:12 +0700

    feat(loading): add anime page loading

--- app/anime/loading.tsx
+++ app/anime/loading.tsx
@@ -1,27 +1,12 @@
-import React from 'react'
+import SkeletonEpisodes from '@/components/skeleton/SkeletonEpisodes'
+import SkeletonHeroBanner from '@/components/skeleton/SkeletonHeroBanner'
 
 function loading() {
   return (
     <>
-      <div className="card max-h-44 min-h-44 min-w-32 rounded sm:max-h-72 sm:min-h-72 sm:min-w-52 skeleton"></div>
-      <div className="p-4 mt-[5rem] sm:mt-[8rem]">
-        <div className="flex embla__container gap-4 relative w-full overflow-x-scroll no-scrollbar">
-          {
-            Array(10).fill(0).map((i: number) => (
-              <div key={i} className="card max-h-44 min-h-44 min-w-32 rounded sm:max-h-72 sm:min-h-72 sm:min-w-52 skeleton"></div>
-            ))
-          }
-        </div>
-      </div>
-      <div className="p-4 mt-[5rem] sm:mt-[8rem]">
-        <div className="flex embla__container gap-4 relative w-full overflow-x-scroll no-scrollbar">
-          {
-            Array(10).fill(0).map((i: number) => (
-              <div key={i} className="card max-h-44 min-h-44 min-w-32 rounded sm:max-h-72 sm:min-h-72 sm:min-w-52 skeleton"></div>
-            ))
-          }
-        </div>
-      </div>
+      <SkeletonHeroBanner />
+      <SkeletonEpisodes />
+      <SkeletonEpisodes />
     </>
   )
 }
