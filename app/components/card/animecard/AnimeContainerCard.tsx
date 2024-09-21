169901205163b5c3a7d117a4853771162b5b94c4
alfaruqii <bumbblechover@gmail.com>
2024-09-21 22:29:39 +0700

    feat(component): update abstraction inside AnimeContainerCard for relations

--- app/components/card/animecard/AnimeContainerCard.tsx
+++ app/components/card/animecard/AnimeContainerCard.tsx
@@ -1,29 +1,35 @@
 "use client";
+
 import { useThemeStore } from '@/store/themeStore'
 import { AnimeCard } from './AnimeCard'
-import { AnimeType } from '@/types/anime.type'
+import { AnimeType, Relation } from '@/types/anime.type'
 
 interface AnimeContainerProps {
-  animes: AnimeType[]
+  animes: AnimeType[] | Relation[];
   containerTitle: string
+}
+
+// Type guard function
+function isRelationArray(arr: AnimeType[] | Relation[]): arr is Relation[] {
+  return 'relation' in arr[0];
 }
 
 export const AnimeContainerCard = ({ animes, containerTitle }: AnimeContainerProps) => {
   const { theme } = useThemeStore();
+
   return (
-    <div className="p-4 overflow-hidden">
-      <p className={`mb-4 pb-2 font-bold border-b ${theme === "garden" ? "border-black" : "border-gray-200"}`}>{containerTitle}</p>
+    <div className={`overflow-hidden ${isRelationArray(animes) ? "p-4" : "sm:p-4"}`}>
+      <p className={`mb-4 pb-2 font-bold border-b ${theme === "garden" ? "border-black" : "border-gray-200"}`}>
+        {containerTitle}
+      </p>
       <div className="flex embla__container gap-4 relative w-full overflow-x-scroll scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-gray-800 scrollbar-track-gray-300 pb-2">
-        {
-          animes.map((anime) => (
-            <AnimeCard
-              key={anime.id}
-              anime={anime}
-            />
-          ))
-        }
+        {animes.map((item) => (
+          <AnimeCard
+            key={item.id}
+            anime={item as AnimeType}
+          />
+        ))}
       </div>
     </div>
   )
 }
-
