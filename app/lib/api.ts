eda6ad047c7b7451ae060c40eb466eec139de284
alfaruqii <bumbblechover@gmail.com>
2024-09-21 14:06:58 +0700

    feat(lib): update api fallback when trying access env

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 import { getEnv } from "../utils/getEnv";
 
 export const API_V1 = ({ headers = {}, params = {} } = {}): AxiosInstance => {
-  const BASE_URL = getEnv('PUBLIC_WATCHLO_API_V1');
+  const BASE_URL = getEnv('WATCHLO_API_V1') ?? process.env["NEXT_PUBLIC_WATCHLO_API_V1"];
 
   const instance = axios.create({
     baseURL: `${BASE_URL}`,
@@ -17,7 +17,7 @@ export const API_V1 = ({ headers = {}, params = {} } = {}): AxiosInstance => {
 }
 
 export const API_V2 = ({ headers = {}, params = {} } = {}): AxiosInstance => {
-  const BASE_URL = getEnv('PUBLIC_WATCHLO_API_V2');
+  const BASE_URL = getEnv('WATCHLO_API_V2') ?? process.env["NEXT_PUBLIC_WATCHLO_API_V2"];
 
   const instance = axios.create({
     baseURL: `${BASE_URL}`,
