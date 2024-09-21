b16323bcc68cc6f42d99088f916a616d270a14c7
alfaruqii <bumbblechover@gmail.com>
2024-09-21 22:39:02 +0700

    feat(component): update SearchedResult(styling)

--- app/components/search/SearchedResult.tsx
+++ app/components/search/SearchedResult.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 import { SearchedAnime } from "@/types/anime.type";
 import Link from "next/link";
 import { useThemeStore } from "@/store/themeStore";
+import { useModalStore } from "@/store/modalStore";
 
 function SearchedResult(props: SearchedAnime) {
+  const { closeModal } = useModalStore();
   const { theme } = useThemeStore();
   const imgSrc = props.coverImage?.large ?? props.coverImage?.medium ?? props.coverImage?.color ?? props.bannerImage;
   const alt = props.title?.userPreferred ?? props.title?.english ?? props.title?.native ?? props.title?.romaji;
   return (
-    <Link href={`/anime/detail/${props.id}`} className={`${theme === "garden" ? "hover:bg-gray-400 " : "hover:bg-gray-800"} rounded p-3`}>
+    <Link onClick={closeModal} href={`/anime/detail/${props.id}`} className={`${theme === "garden" ? "hover:bg-gray-400 " : "hover:bg-gray-800"} rounded p-3`}>
       <div className="w-full flex gap-2">
         <div className="relative w-32 h-36">
           <Image
