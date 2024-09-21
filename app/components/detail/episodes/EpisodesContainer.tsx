12f922567afd8d89ebe2d66941c5dafeb3d1ae93
alfaruqii <bumbblechover@gmail.com>
2024-09-21 22:33:39 +0700

    feat(component): add functionality change to dubbing[EpisodesContainer] 🗣️

--- app/components/detail/episodes/EpisodesContainer.tsx
+++ app/components/detail/episodes/EpisodesContainer.tsx
@@ -1,22 +1,87 @@
-import { AnimeServiceV1 } from "@/services";
+"use client";
+
+import { useState, useEffect } from "react";
 import { AnimeDetails, AnimeInfo } from "@/types/anime.type"
 import { AnimeCardDetail } from "../card/AnimeCardDetail";
+import SkeletonEpisodes from "@/components/skeleton/SkeletonEpisodes";
+import { useThemeStore } from "@/store/themeStore";
 
-async function EpisodesContainer(anime: AnimeInfo) {
-  const { data: dataV1Info }: { data: AnimeDetails } = await AnimeServiceV1.getAnimeInfoV1(anime?.id_provider?.idGogo);
-  return (
-    <>
-      <div className="py-4 overflow-hidden">
-        <div className="flex embla__container gap-4 relative w-full overflow-x-scroll scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-gray-800 scrollbar-track-gray-300 pb-2">
-          {
-            dataV1Info.episodes.map((episode) =>
-              <AnimeCardDetail key={episode.id} animeImage={dataV1Info.image} id={dataV1Info.id} episodeNumber={episode.number} episodeId={episode.id} />
-            )
+function EpisodesContainer(anime: AnimeInfo) {
+  const { theme } = useThemeStore();
+  const [provider, setProvider] = useState<string | undefined>(anime.id_provider?.idGogo);
+  const [episodes, setEpisodes] = useState<AnimeDetails | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchEpisodes() {
+      if (provider) {
+        setLoading(true);
+        try {
+          const response = await fetch(`/api/anime-infov1?query=${provider}`);
+          if (!response.ok) {
+            throw new Error('Failed to fetch search results')
           }
-        </div>
+          const data = await response.json()
+          setEpisodes(data);
+        } catch (error) {
+          console.error("Failed to fetch episodes:", error);
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+
+    fetchEpisodes();
+  }, [provider]);
+
+  return (
+    <div className="overflow-hidden sm:px-4 py-4">
+      <div className="mb-4 flex gap-1">
+        {
+          anime.id_provider?.idGogoDub && (
+            <button
+              type="button"
+              className={`${provider === anime.id_provider?.idGogo ? `bg-gray-400 ${theme !== "garden" ? "text-gray-800" : ""}` : ``} btn btn-sm`}
+              onClick={() => setProvider(anime.id_provider?.idGogo)}
+              disabled={!anime.id_provider?.idGogo}
+            >
+              Sub
+            </button>
+          )
+        }
+        {
+          anime.id_provider?.idGogoDub && (
+            <button
+              type="button"
+              className={`${provider === anime.id_provider?.idGogoDub ? `bg-gray-400 ${theme !== "garden" ? "text-gray-800" : ""}` : ''} btn btn-sm`}
+              onClick={() => setProvider(anime.id_provider?.idGogoDub)}
+              disabled={!anime.id_provider?.idGogoDub}
+            >
+              Dub
+            </button>
+          )
+        }
       </div>
-    </>
-  )
+
+      {loading ? (
+        <SkeletonEpisodes noMargin />
+      ) : episodes ? (
+        <div className="embla__container scrollbar-thumb-rounded-full scrollbar-track-rounded-full relative flex w-full gap-4 overflow-x-scroll pb-2 scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-gray-800">
+          {episodes.episodes.map((episode) => (
+            <AnimeCardDetail
+              key={episode.id}
+              animeImage={episodes.image}
+              id={episodes.id}
+              episodeNumber={episode.number}
+              episodeId={episode.id}
+            />
+          ))}
+        </div>
+      ) : (
+        <div>No episodes available</div>
+      )}
+    </div>
+  );
 }
 
 export default EpisodesContainer;
