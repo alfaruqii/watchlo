c2fd7402458550acb38110f103be7b5247367d78
alfaruqii <bumbblechover@gmail.com>
2024-09-23 16:37:48 +0700

    feat(component): add Hamburger Navbar and Menubar state[Navbar]

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,12 +1,16 @@
 "use client";
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import { Spin as Hamburger } from 'hamburger-react'
 import { ToggleTheme } from "./ToggleTheme";
 import { useThemeStore } from "@/store/themeStore";
 import ToggleSearch from "./ToggleSearch";
+import Menu from "./Menu";
 
 export const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isOpen, setOpen] = useState(false);
+
   const { theme } = useThemeStore();
 
   // Add event listener for scroll event
@@ -27,8 +31,9 @@ export const Navbar = () => {
 
   return (
     <>
+      <Menu isToggled={isOpen} />
       <div
-        className={`${isScrolled ? theme === "garden" ? "bg-base-100/80 backdrop-blur-lg" : "bg-black/80 backdrop-blur-lg" : "bg-base-100"} sticky top-0 z-[90] flex items-center justify-between px-4 py-2 drop-shadow-lg transition-all duration-300
+        className={`${isScrolled ? theme === "garden" ? "bg-base-100/80 backdrop-blur-lg" : "bg-black/80 backdrop-blur-lg" : "bg-base-100"} sticky top-0 z-[90] flex items-center justify-between px-4 sm:py-2 drop-shadow-lg transition-all duration-300
           sm:px-12`}
       >
         <div className={`drop-shadow-lg ${theme === "black" ? "text-white" : "text-black"}`}>
@@ -39,7 +44,7 @@ export const Navbar = () => {
         </div>
         <div className={`drop-shadow-lg hidden sm:flex gap-10 font-normal ${theme === "black" ? "text-white" : "text-black"}`}>
           <Link href="/">
-            <span className="">All</span>
+            <span className="">Movies</span>
           </Link>
           <Link href="/anime">
             <span className="">Anime</span>
@@ -48,10 +53,19 @@ export const Navbar = () => {
             <span className="text-gray-500 pr-5">Manga</span>
             <div className={`indicator-item font-bold px-1 w-fit badge rounded indicator-top mt-1 indicator-end text-xs ${theme === "black" ? "border-gray-400" : "border-gray-400"}`}>DEV</div>
           </Link>
+          <Link href="/">
+            <span className="">Docs</span>
+          </Link>
+          <Link href="https://github.com/alfaruqii/watchlo" target="_blank">
+            <span className="">Github</span>
+          </Link>
         </div>
         <div className="flex items-center gap-2">
           <ToggleSearch />
           <ToggleTheme />
+          <div className="sm:hidden">
+            <Hamburger toggled={isOpen} toggle={setOpen} duration={0.6} rounded size={25} />
+          </div>
         </div>
       </div>
     </>
