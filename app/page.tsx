e626febf5162d6c2b53200692636d6a987b8ebf5
alfaruqii <bumbblechover@gmail.com>
2024-09-25 15:47:40 +0700

    feat(page)[Movies]: change the Hero to HeroMediaCarousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import HeroMediaCarousel from "./components/hero/HeroMediaCarousel";
+import { MovieService } from "./services";
+
 export default async function Home() {
+  const { data: dataMoviePopular } = await MovieService.getPopularMovies();
 
   return (
     <div>
+      <HeroMediaCarousel items={dataMoviePopular.results} />
     </div>
   );
 }
