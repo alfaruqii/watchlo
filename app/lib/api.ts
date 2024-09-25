87ee3ba6f436066af5f7016562dacfb762857e25
alfaruqii <bumbblechover@gmail.com>
2024-09-25 23:17:29 +0700

    feat(api): add API_V0 for movies

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,6 +1,21 @@
 import axios, { AxiosInstance } from "axios";
 import { getEnv } from "../utils/getEnv";
 
+export const API_V0 = ({ headers = {}, params = {} } = {}): AxiosInstance => {
+  const BASE_URL = getEnv('WATCHLO_API_V0') ?? process.env["NEXT_PUBLIC_WATCHLO_API_V0"];
+
+  const instance = axios.create({
+    baseURL: `${BASE_URL}`,
+    headers: {
+      'Content-type': 'application/json',
+      ...headers,
+    },
+    params,
+  });
+
+  return instance;
+}
+
 export const API_V1 = ({ headers = {}, params = {} } = {}): AxiosInstance => {
   const BASE_URL = getEnv('WATCHLO_API_V1') ?? process.env["NEXT_PUBLIC_WATCHLO_API_V1"];
 
