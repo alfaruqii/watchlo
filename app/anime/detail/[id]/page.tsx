dfaa4bea83d697f570c18b67fcd2addd2c48ad04
alfaruqii <bumbblechover@gmail.com>
2024-09-26 19:15:39 +0700

    feat(page): update specific detail page[anime]

--- app/anime/detail/[id]/page.tsx
+++ app/anime/detail/[id]/page.tsx
@@ -3,13 +3,13 @@ import { Suspense } from 'react';
 import { Banner } from '@/components/detail/banner/Banner';
 import CardBanner from '@/components/detail/cardbanner/CardBanner';
 import EpisodesContainer from '@/components/detail/episodes/EpisodesContainer';
+import RelationComponent from '@/components/detail/relation/RelationComponent';
 import InfoDetails from '@/components/detail/infodetails/InfoDetails';
 import Trailer from '@/components/media/Trailer';
 import SkeletonEpisodes from '@/components/skeleton/SkeletonEpisodes';
 
 import { AnimeServiceV2 } from '@/services';
 import { AnimeInfo } from '@/types/anime.type';
-import RelationComponent from '@/components/detail/relation/RelationComponent';
 
 async function DetailPage({ params }: { params: { id: string } }) {
   const { id } = params;
@@ -18,10 +18,10 @@ async function DetailPage({ params }: { params: { id: string } }) {
   return (
     <>
       <div>
-        <Banner {...dataInfo} />
+        <Banner item={dataInfo} />
         <div className="p-6">
-          <CardBanner {...dataInfo} />
-          <InfoDetails {...dataInfo} />
+          <CardBanner item={dataInfo} />
+          <InfoDetails item={dataInfo} />
           <Trailer trailer={dataInfo.trailer} />
           <Suspense
             fallback={<SkeletonEpisodes />}>
