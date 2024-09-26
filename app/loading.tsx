0ffe3487d621973586181e90fa23d798693e21b0
alfaruqii <bumbblechover@gmail.com>
2024-09-26 19:41:34 +0700

    feat(loading): create loading for root page

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+import SkeletonEpisodes from '@/components/skeleton/SkeletonEpisodes'
+import SkeletonHeroBanner from '@/components/skeleton/SkeletonHeroBanner'
+
+function loading() {
+  return (
+    <>
+      <SkeletonHeroBanner />
+      <SkeletonEpisodes />
+      <SkeletonEpisodes />
+      <SkeletonEpisodes />
+    </>
+  )
+}
+
+export default loading
