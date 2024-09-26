acdadb4a67a2aec3a8c546f2858edbf2252ab230
alfaruqii <bumbblechover@gmail.com>
2024-09-26 19:43:01 +0700

    feat(page): update root page(add Movie and TV, either Popular 🥇 or Top Rated 💹)

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
+import { MoviesContainerCard } from "./components/card/moviescard/MoviesContainterCard";
 import HeroMediaCarousel from "./components/hero/HeroMediaCarousel";
 import { MovieService } from "./services";
 
 export default async function Home() {
   const { data: dataMoviePopular } = await MovieService.getPopularMovies();
+  const { data: dataMovieTopRated } = await MovieService.getMoviesTopRated();
+  const { data: dataTvPopular } = await MovieService.getPopularTV();
+  const { data: dataTvTopRated } = await MovieService.getTvTopRated();
 
   return (
-    <div>
-      <HeroMediaCarousel items={dataMoviePopular.results} />
-    </div>
+    <>
+      <div className="flex flex-col gap-4 min-h-fit">
+        <HeroMediaCarousel items={dataMoviePopular.results} />
+        <div className="p-4 sm:p-0">
+          <MoviesContainerCard containerTitle="Top Rated Movie 🥇" movies={dataMovieTopRated.results} />
+        </div>
+        <div className="p-4 sm:p-0">
+          <MoviesContainerCard containerTitle="Top Rated Series 🏆" movies={dataTvTopRated.results} />
+        </div>
+        <div className="p-4 sm:p-0">
+          <MoviesContainerCard containerTitle="Popular Series 💯" movies={dataTvPopular.results} />
+        </div>
+      </div>
+    </>
   );
 }
 
