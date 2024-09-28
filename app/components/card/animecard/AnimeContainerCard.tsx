2a7d8ec760c6fcf97e213656d24b433bc369450b
alfaruqii <bumbblechover@gmail.com>
2024-09-28 15:27:23 +0700

    feat(component): change few line styling[AnimeContainerCard]

--- app/components/card/animecard/AnimeContainerCard.tsx
+++ app/components/card/animecard/AnimeContainerCard.tsx
@@ -14,7 +14,7 @@ export const AnimeContainerCard = ({ animes, containerTitle }: AnimeContainerPro
 
   return (
     <div className="overflow-hidden sm:p-4">
-      <p className={`mb-4 pb-2 font-bold border-b ${theme === "garden" ? "border-black" : "border-gray-200"}`}>
+      <p className={`mb-4 pb-1 font-bold border-b ${theme === "garden" ? "border-black" : "border-gray-200"}`}>
         {containerTitle}
       </p>
       <div className="flex embla__container gap-4 relative w-full overflow-x-scroll scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-gray-800 scrollbar-track-gray-300 pb-2">
