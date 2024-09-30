c9a75e7ceeb04a9553e88aa58e353f4f4517b249
alfaruqii <bumbblechover@gmail.com>
2024-09-30 19:11:39 +0700

    feat(component): update ButtonWatch(add condition where is not yet released, and navigating functionality)

--- app/components/season/ButtonWatch.tsx
+++ app/components/season/ButtonWatch.tsx
@@ -1,7 +1,16 @@
-function ButtonWatch({ text }: { text: string }) {
+import Link from "next/link"
+
+function ButtonWatch({ text, season, ep = 1, id }: { text: string; season?: number; ep?: number; id?: number; isAnime?: boolean }) {
+  const routes = `/series/watch/${id}/season/${season}/ep/${ep}`;
   return (
     <>
-      <button type="button" className="btn btn-sm w-fit rounded bg-gray-400 border-none font-bold text-gray-800 md:text-lg">{text}</button>
+      {
+        id ?
+          <Link href={routes}>
+            <button type="button" className="btn btn-sm w-fit rounded bg-gray-400 border-none font-bold text-gray-900 md:text-lg">{text}</button>
+          </Link> :
+          <button type="button" className={`btn btn-sm w-fit rounded bg-gray-400 border-none font-bold text-gray-900 md:text-lg ${text.toLowerCase().includes("not yet released") ? "pointer-events-none" : ""}`}>{text}</button>
+      }
     </>
   )
 }
