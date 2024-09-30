e077c41561d90238026f7362643625f41e8a7b30
alfaruqii <bumbblechover@gmail.com>
2024-09-30 19:15:52 +0700

    feat(component): update EpisodesComponent(handling condition for series)

--- app/components/watch/episodes/EpisodesComponent.tsx
+++ app/components/watch/episodes/EpisodesComponent.tsx
@@ -1,21 +1,33 @@
-"use client";
-import { AnimeDetails, Episode } from "@/types/anime.type"
+import { AnimeDetails, AnimeEpisode } from "@/types/anime.type"
 import EpisodeComponent from "./EpisodeComponent"
+import { SeriesEpisode, TV } from "@/types/movies.type";
+import { isAnimeDetails } from "@/utils/mediaTypeChecker";
 
-interface Anime extends AnimeDetails {
-  ep: string
+interface Item {
+  item: AnimeDetails | TV;
+  ep: number;
+  season?: number;
+  id?: string;
 }
 
-function EpisodesComponent(anime: Anime) {
+function EpisodesComponent(data: Item) {
   return (
     <>
-      <div className="lg:max-w-[50%] flex flex-col pt-1.5">
-        <p className="pb-1">
-          Now Playing Episode {anime.ep}
-        </p>
-        <div className="flex flex-wrap gap-2 scrollbar scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-gray-800 scrollbar-track-gray-300 max-h-96 2xl:max-h-[30rem] overflow-y-scroll ">
+      <div className="flex flex-col pt-1.5 lg:max-w-[50%]">
+        {
+          isAnimeDetails(data.item) ?
+            <p className="pb-1">
+              Now Playing Episode {data.ep}
+            </p> :
+            <p className="pb-1">
+              Now Playing (Season {data.season} - Episode {data.ep})
+            </p>
+        }
+        <div className="scrollbar-thumb-rounded-full scrollbar-track-rounded-full flex max-h-96 flex-wrap gap-2 overflow-y-scroll scrollbar scrollbar-track-gray-300 scrollbar-thumb-gray-800 2xl:max-h-[30rem] ">
           {
-            anime.episodes.map((i: Episode) => <EpisodeComponent key={i.id} id={i.id} titleAndEpisode={anime.id as string} episodeNumber={i.number} epQuery={anime.ep} />)
+            isAnimeDetails(data.item) ?
+              data.item.episodes.map((i: AnimeEpisode) => <EpisodeComponent key={i.id} id={i.id} titleAndEpisode={data.item.id as string} episodeNumber={i.number} epQuery={data.ep} />) :
+              data.item.episodes.map((i: SeriesEpisode) => <EpisodeComponent key={i.id} id={String(data.id)} titleAndEpisode={data.item.id as string} episodeNumber={i.episode_number} epQuery={data.ep} isSeries season={i.season_number} ep={i.episode_number} />)
           }
         </div>
       </div>
