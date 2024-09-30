7c493f935a6cb9ba5d81c83374892dd770b3049c
alfaruqii <bumbblechover@gmail.com>
2024-09-30 19:05:53 +0700

    feat(page): update specific anime page

--- app/anime/watch/[id]/page.tsx
+++ app/anime/watch/[id]/page.tsx
@@ -5,23 +5,23 @@ import { AnimeDetails, StreamInfo } from '@/types/anime.type';
 
 type WatchPageParams = {
   params: { id: string };
-  searchParams: { animeTitle: string; ep: string };
+  searchParams: { title: string; ep: number };
 }
 
 async function WatchPage({ params, searchParams }: WatchPageParams) {
   const { id } = params;
-  const { animeTitle, ep } = searchParams;
+  const { title, ep } = searchParams;
 
   // Fetch the stream info (for video sources)
   const { data: dataStream }: { data: StreamInfo } = await AnimeServiceV1.getAnimeStream(`${id}`);
   // Fetch the anime details (for title, image, etc.)
-  const { data: dataInfo }: { data: AnimeDetails } = await AnimeServiceV1.getAnimeInfoV1(`${animeTitle}`);
+  const { data: dataInfo }: { data: AnimeDetails } = await AnimeServiceV1.getAnimeInfoV1(`${title}`);
 
   return (
     <>
       <div className="flex flex-col gap-6 p-6 lg:flex-row">
         <Media title={dataInfo.title} poster={dataInfo.image} {...dataStream} />
-        <EpisodesComponent {...dataInfo} ep={ep} />
+        <EpisodesComponent item={dataInfo} ep={ep} />
       </div>
 
     </>
