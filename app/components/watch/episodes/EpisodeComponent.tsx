fec257253902fc5f559465e1aec7f8690a9fbf63
alfaruqii <bumbblechover@gmail.com>
2024-09-30 19:13:34 +0700

    feat(component): update EpisodeComponent(handling condition for series)

--- app/components/watch/episodes/EpisodeComponent.tsx
+++ app/components/watch/episodes/EpisodeComponent.tsx
@@ -1,22 +1,29 @@
 "use client";
 import { useThemeStore } from "@/store/themeStore";
+import { Routes } from "@/types/global";
 import Link from "next/link"
 
 type Episode = {
-  id: string;
-  epQuery: string;
+  epQuery: number;
   titleAndEpisode: string;
   episodeNumber: number;
+  isSeries?: boolean;
+  id: string;
+  season?: number;
+  ep?: number;
 }
 
-function EpisodeComponent({ id, titleAndEpisode, episodeNumber, epQuery }: Episode) {
+function EpisodeComponent({ id, titleAndEpisode, episodeNumber, epQuery, isSeries, season = 1, ep = 1 }: Episode) {
   const { theme } = useThemeStore();
-  const route = { pathname: `/anime/watch/${id}`, query: { animeTitle: titleAndEpisode, ep: episodeNumber } }
+  const determineRoutes = (): Routes | string => {
+    if (isSeries) return `/series/watch/${id}/season/${season}/ep/${ep}`
+    return { pathname: `/anime/watch/${id}`, query: { title: titleAndEpisode, ep: episodeNumber } };
+  }
 
-  const conditionEpisode = String(episodeNumber) === epQuery;
+  const conditionEpisode = String(episodeNumber) === String(epQuery);
   return (
     <>
-      <Link href={route}>
+      <Link href={determineRoutes()}>
         <div className={`btn min-w-24 2xl:min-w-24 ${conditionEpisode ? `${theme === "garden" ? "bg-gray-700 text-white" : "bg-gray-600"}` : ""}`}>
           <p>
             {
