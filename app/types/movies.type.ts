7b2e9197bbdc74db061d4073d67df4a9f871edf8
alfaruqii <bumbblechover@gmail.com>
2024-10-01 11:57:59 +0700

    feat(type): add new type for Review

--- app/types/movies.type.ts
+++ app/types/movies.type.ts
@@ -155,3 +155,22 @@ export interface TV {
   vote_average: number;
 }
 
+export interface AuthorDetails {
+  name: string;
+  username: string;
+  avatar_path: string | null;
+  rating: number;
+}
+
+
+export interface Review {
+  id: string;
+  author: string;
+  author_details: AuthorDetails;
+  content: string;
+  created_at: string;
+  updated_at: string;
+  url: string;
+  rating?: number;
+}
+
