1e27b174dda29597374a6a1721f61ad3eb6b73a6
alfaruqii <bumbblechover@gmail.com>
2024-10-01 12:20:32 +0700

    feat(page): add data for reviews[movie(detail)]

--- app/movie/detail/[id]/page.tsx
+++ app/movie/detail/[id]/page.tsx
@@ -6,14 +6,16 @@ import InfoDetails from '@/components/detail/infodetails/InfoDetails';
 import SkeletonEpisodes from '@/components/skeleton/SkeletonEpisodes';
 
 import { MovieService } from '@/services';
-import { MovieInfo, Video } from '@/types/movies.type';
+import { MovieInfo, Review, Video } from '@/types/movies.type';
 import Trailer from '@/components/media/Trailer';
 import Embeded from '@/components/media/Embeded';
+import ReviewsComponent from '@/components/reviews/ReviewsComponent';
 
 async function DetailPage({ params }: { params: { id: string } }) {
   const { id } = params;
   const { data: dataInfo }: { data: MovieInfo } = await MovieService.getMoviesById(id);
   const { data: { results: dataVideos } }: { data: { results: Video[] } } = await MovieService.getMovieTrailer(id);
+  const { data: { results: dataReviews } }: { data: { results: Review[] } } = await MovieService.getMovieReviews(id);
   const trailerVideo = dataVideos.find((video: Video) => (video.type.toLowerCase() === "trailer" || video.type.toLowerCase() === "trailer"))
 
   return (
@@ -33,6 +35,10 @@ async function DetailPage({ params }: { params: { id: string } }) {
               <Embeded type="movie" id={id} />
             </Suspense>
           </div>
+          {
+            dataReviews.length > 0 &&
+            <ReviewsComponent reviews={dataReviews} />
+          }
         </div>
       </div>
     </>
