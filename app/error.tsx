9336b366591eb02e3dfd4d2e7ef5211e0f12462f
alfaruqii <bumbblechover@gmail.com>
2024-10-04 23:56:55 +0700

    chore(error): make the image unoptimized(for quota limitation purpose)

--- app/error.tsx
+++ app/error.tsx
@@ -5,7 +5,7 @@ function Error() {
   return (
     <>
       <div className="flex min-h-screen flex-col items-center justify-center gap-2 px-4">
-        <Image src="/fallback-card.webp" alt="Windah Batubara" className="rounded drop-shadow-lg" width={200} height={200} />
+        <Image unoptimized src="/fallback-card.webp" alt="Windah Batubara" className="rounded drop-shadow-lg" width={200} height={200} />
         <p className="text-center text-lg">There is an error :/ please contact me if you were concern at my github, or create an issue, thanks :]</p>
       </div>
     </>
