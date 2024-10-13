1b9c01fd651939fa00a600516db21667729fb906
alfaruqii <bumbblechover@gmail.com>
2024-10-13 19:18:31 +0700

    feat(page): update root paga(add trending(both for movie & series), and update convention naming method services)

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,27 @@ import HeroMediaCarousel from "./components/hero/HeroMediaCarousel";
 import { MovieService } from "./services";
 
 export default async function Home() {
-  const { data: dataMoviePopular } = await MovieService.getPopularMovies();
-  const { data: dataMovieTopRated } = await MovieService.getMoviesTopRated();
-  const { data: dataTvPopular } = await MovieService.getPopularTV();
+  const { data: dataMoviesPopular } = await MovieService.getMoviesPopular();
+  const { data: dataMoviesTopRated } = await MovieService.getMoviesTopRated();
+  const { data: dataMoviesTrending } = await MovieService.getMoviesTrending();
   const { data: dataTvTopRated } = await MovieService.getTvTopRated();
+  const { data: dataTvTrending } = await MovieService.getTvTrending();
 
   return (
     <>
       <div className="flex flex-col gap-4 min-h-fit">
-        <HeroMediaCarousel items={dataMoviePopular.results} />
+        <HeroMediaCarousel items={dataMoviesPopular.results} />
         <div className="p-4 sm:p-0">
-          <MoviesContainerCard containerTitle="Top Rated Movie 🥇" movies={dataMovieTopRated.results} />
+          <MoviesContainerCard containerTitle="Top Rated Movie 🥇" movies={dataMoviesTopRated.results} />
+        </div>
+        <div className="p-4 sm:p-0">
+          <MoviesContainerCard containerTitle="Trending Movies 📈" movies={dataMoviesTrending.results} />
         </div>
         <div className="p-4 sm:p-0">
           <MoviesContainerCard containerTitle="Top Rated Series 🏆" movies={dataTvTopRated.results} />
         </div>
         <div className="p-4 sm:p-0">
-          <MoviesContainerCard containerTitle="Popular Series 💯" movies={dataTvPopular.results} />
+          <MoviesContainerCard containerTitle="Trending Series 🔥" movies={dataTvTrending.results} />
         </div>
       </div>
     </>
