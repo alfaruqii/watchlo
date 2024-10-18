0d1a9a1dfd7b5915e5364b8f971c9eaabdeef78f
alfaruqii <bumbblechover@gmail.com>
2024-10-18 22:37:51 +0700

    feat(page): code formatting(prettier)

--- app/anime/page.tsx
+++ app/anime/page.tsx
@@ -4,7 +4,7 @@ import { AnimeServiceV1, AnimeServiceV2 } from "../services";
 
 export default async function Home() {
   const { data: dataTrending } = await AnimeServiceV2.getTrendingAnime();
-  const { data: dataRecent } = await AnimeServiceV1.getRecentEpisode();
+  const { data: dataRecent } = await AnimeServiceV1.getRecentEpisodeGogo();
   const { data: dataPopular } = await AnimeServiceV2.getPopularAnime();
 
   return (
@@ -12,14 +12,18 @@ export default async function Home() {
       <div className="flex flex-col gap-4 min-h-fit">
         <HeroMediaCarousel items={dataTrending.results} />
         <div className="p-4 sm:p-0">
-          <AnimeContainerCard containerTitle="Recently Updated 🎬" animes={dataRecent.results} />
+          <AnimeContainerCard
+            containerTitle="Recently Updated 🎬"
+            animes={dataRecent.results}
+          />
         </div>
         <div className="p-4 sm:p-0">
-          <AnimeContainerCard containerTitle="Most Popular 💯" animes={dataPopular.results} />
+          <AnimeContainerCard
+            containerTitle="Most Popular 💯"
+            animes={dataPopular.results}
+          />
         </div>
       </div>
     </>
   );
 }
-
-
