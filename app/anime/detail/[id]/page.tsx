5a4b61c1a8b127c3e0e8d9d2aad6a20340d427b1
alfaruqii <bumbblechover@gmail.com>
2024-10-19 23:48:29 +0700

    feat(page): code formatting(prettier)

--- app/anime/detail/[id]/page.tsx
+++ app/anime/detail/[id]/page.tsx
@@ -1,19 +1,25 @@
-import { Suspense } from 'react';
+import { Suspense } from "react";
 
-import { Banner } from '@/components/detail/banner/Banner';
-import CardBanner from '@/components/detail/cardbanner/CardBanner';
-import EpisodesContainer from '@/components/detail/episodes/EpisodesContainer';
-import RelationComponent from '@/components/detail/relation/RelationComponent';
-import InfoDetails from '@/components/detail/infodetails/InfoDetails';
-import Trailer from '@/components/media/Trailer';
-import SkeletonEpisodes from '@/components/skeleton/SkeletonEpisodes';
+import { Banner } from "@/components/detail/banner/Banner";
+import CardBanner from "@/components/detail/cardbanner/CardBanner";
+import EpisodesContainer from "@/components/detail/episodes/EpisodesContainer";
+import RelationComponent from "@/components/detail/relation/RelationComponent";
+import InfoDetails from "@/components/detail/infodetails/InfoDetails";
+import Trailer from "@/components/media/Trailer";
+import SkeletonEpisodes from "@/components/skeleton/SkeletonEpisodes";
 
-import { AnimeServiceV2 } from '@/services';
-import { AnimeInfo } from '@/types/anime.type';
+import { AnimeServiceV2 } from "@/services";
+import { AnimeInfo, RelationOrRecommendation } from "@/types/anime.type";
+import { AnimeContainerCard } from "@/components/card/animecard/AnimeContainerCard";
 
 async function DetailPage({ params }: { params: { id: string } }) {
   const { id } = params;
-  const { data: dataInfo }: { data: AnimeInfo } = await AnimeServiceV2.getAnimeInfoV2(id);
+  const { data: dataInfo }: { data: AnimeInfo } =
+    await AnimeServiceV2.getAnimeInfoV2(id);
+  const {
+    data: { results: dataRecommendations },
+  }: { data: { results: RelationOrRecommendation[] } } =
+    await AnimeServiceV2.getRecommendationAnime(id);
 
   return (
     <>
@@ -23,18 +29,25 @@ async function DetailPage({ params }: { params: { id: string } }) {
           <CardBanner item={dataInfo} />
           <InfoDetails item={dataInfo} />
           <Trailer trailer={dataInfo.trailer} />
-          <Suspense
-            fallback={<SkeletonEpisodes />}>
-            {
-              (dataInfo.id_provider?.idGogo || dataInfo.id_provider?.idGogoDub) &&
+          <Suspense fallback={<SkeletonEpisodes />}>
+            {(dataInfo.id_provider?.idGogo ||
+              dataInfo.id_provider?.idGogoDub) && (
               <EpisodesContainer {...dataInfo} />
-            }
+            )}
           </Suspense>
-          <RelationComponent relation={dataInfo.relation} />
+          <div className="flex flex-col gap-4">
+            <RelationComponent relation={dataInfo.relation} />
+            {dataRecommendations.length > 0 && (
+              <AnimeContainerCard
+                animes={dataRecommendations}
+                containerTitle="Recommendations 👌"
+              />
+            )}
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default DetailPage;
