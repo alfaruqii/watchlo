0dd21605416fd9410ccb9cf1e2566294fe73a19d
alfaruqii <bumbblechover@gmail.com>
2024-10-25 17:49:14 +0700

    feat(component): update ReviewsComponent(update onLoad blur on reviewer photo profile)

--- app/components/reviews/ReviewsComponent.tsx
+++ app/components/reviews/ReviewsComponent.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { marked } from "marked";
 import parse from "html-react-parser";
 import { formatDate } from "@/utils/formatted";
-import fallbackContent from "@/utils/fallbackDesc.json"
+import fallbackContent from "@/utils/fallbackDesc.json";
 import { useThemeStore } from "@/store/themeStore";
 import { Review } from "@/types/movies.type";
 
@@ -16,41 +16,72 @@ function cleanReviewContent(content: string | undefined) {
 }
 
 function ReviewsComponent({ reviews }: { reviews: Review[] }) {
-  const [isImageLoading, setImageLoading] = useState(true);
+  const [isImageLoading, setImageLoading] = useState<boolean>(true);
   const { theme } = useThemeStore();
   // Filter out reviews that have author details
-  const filteredReviews = reviews.filter((review: Review) =>
-    review.author_details.avatar_path || review.author_details.name || review.author_details.username || review.content
+  const filteredReviews = reviews.filter(
+    (review: Review) =>
+      review.author_details.avatar_path ||
+      review.author_details.name ||
+      review.author_details.username ||
+      review.content
   );
 
   return (
-    <div className="lg:px-72 my-10">
-      <p className={`border-b ${theme === "garden" ? "border-gray-500" : "border-gray-300"} pb-1`}>Reviews</p>
-      <div className="flex flex-col rounded mt-4 gap-8 py-3">
+    <div className="my-10 lg:px-72">
+      <p
+        className={`${
+          theme === "garden" ? "border-gray-500" : "border-gray-300"
+        }
+        border-b pb-1`}
+      >
+        Reviews
+      </p>
+      <div className="mt-4 flex flex-col gap-8 rounded py-3">
         {filteredReviews.map((review: Review, index) => (
-          <div key={index} className={`border-b ${theme === "garden" ? "border-gray-300 " : "border-gray-500"} pb-4`}>
+          <div
+            key={index}
+            className={`${
+              theme === "garden" ? "border-gray-300 " : "border-gray-500"
+            }
+            border-b pb-4`}
+          >
             {/* Author Details */}
-            <div className="flex gap-2 mb-2 items-center">
-              <div className="size-11 lg:size-12 relative rounded-full overflow-hidden">
+            <div className="mb-2 flex items-center gap-2">
+              <div className="relative size-11 overflow-hidden rounded-full lg:size-12">
                 <Image
                   unoptimized
                   fill
-                  src={review.author_details.avatar_path || "/fallback-card.webp"}
+                  src={
+                    review.author_details.avatar_path || "/fallback-card.webp"
+                  }
                   alt={review.author_details.name || "Avatar"}
                   onLoad={() => setImageLoading(false)}
-                  className={`object-cover ${isImageLoading ? 'blur-2xl' : 'blur-0'}`}
+                  className={`object-cover transition-custom-blur  ${
+                    isImageLoading ? "scale-110 blur-2xl" : "scale-100 blur-0"
+                  }
+                  object-cover`}
                 />
               </div>
               <div>
-                <p className="capitalize text-sm font-semibold line-clamp-1">
-                  {review.author_details.name || review.author_details.username || "Windah Batubara"}
+                <p className="line-clamp-1 text-sm font-semibold capitalize">
+                  {review.author_details.name ||
+                    review.author_details.username ||
+                    "Windah Batubara"}
                 </p>
-                <p className="text-xs text-gray-500">{formatDate(review.updated_at)}</p>
+                <p className="text-xs text-gray-500">
+                  {formatDate(review.updated_at)}
+                </p>
               </div>
             </div>
 
             {/* Review Content */}
-            <div className={`text-sm ${theme === "garden" ? "text-gray-700 " : "text-gray-300"} line-clamp-5`}>
+            <div
+              className={`${
+                theme === "garden" ? "text-gray-700 " : "text-gray-300"
+              }
+              line-clamp-5 text-sm`}
+            >
               {cleanReviewContent(review.content)}
             </div>
           </div>
@@ -61,4 +92,3 @@ function ReviewsComponent({ reviews }: { reviews: Review[] }) {
 }
 
 export default ReviewsComponent;
-
