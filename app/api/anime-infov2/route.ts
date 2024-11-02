1c0ef518383eb74221485dd7630cd0236a83210c
alfaruqii <bumbblechover@gmail.com>
2024-11-02 21:06:22 +0700

    feat(api): add new api route(anime infov2)

---
+++ app/api/anime-infov2/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { AnimeServiceV2 } from "@/services";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+
+  if (!query) {
+    return NextResponse.json(
+      { error: "Query parameter is required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const { data } = await AnimeServiceV2.getAnimeInfoV2(query);
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error searching anime:", error);
+    return NextResponse.json(
+      { error: "Failed to search anime" },
+      { status: 500 }
+    );
+  }
+}
