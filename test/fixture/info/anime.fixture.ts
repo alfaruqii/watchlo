b418bb34cbf313cb57299150ac5088cda0ca2e21
alfaruqii <bumbblechover@gmail.com>
2024-11-03 21:16:23 +0700

    feat(test): update fixture

--- test/fixture/info/anime.fixture.ts
+++ test/fixture/info/anime.fixture.ts
@@ -1,42 +1,39 @@
-import { AnimePopular, AnimeTrending } from "@/types/anime.type";
-import { AnimeInfo } from "@/types/anime.type";
-
-export const animeFixture: AnimeInfo = {
+export const animeFixture = {
   id: 1,
   code: 200,
-  message: 'Success',
+  message: "Success",
   idMal: 1,
   id_provider: {
-    idGogo: 'gogo_id',
-    idGogoDub: 'gogo_dub_id',
-    idZoro: 'zoro_id',
-    id9anime: '9anime_id',
-    idPahe: 'pahe_id',
+    idGogo: "gogo_id",
+    idGogoDub: "gogo_dub_id",
+    idZoro: "zoro_id",
+    id9anime: "9anime_id",
+    idPahe: "pahe_id",
   },
   title: {
-    userPreferred: 'Anime Title',
-    romaji: 'Romaji Title',
-    english: 'English Title',
-    native: 'Native Title',
+    userPreferred: "Anime Title",
+    romaji: "Romaji Title",
+    english: "English Title",
+    native: "Native Title",
   },
   dub: true,
-  description: 'This is an anime description.',
+  description: "This is an anime description.",
   coverImage: {
-    large: 'https://example.com/cover-large.jpg',
-    medium: 'https://example.com/cover-medium.jpg',
-    color: 'https://example.com/cover-color.jpg',
+    large: "https://example.com/cover-large.jpg",
+    medium: "https://example.com/cover-medium.jpg",
+    color: "https://example.com/cover-color.jpg",
   },
-  bannerImage: 'https://example.com/banner.jpg',
-  genres: ['Action', 'Adventure'],
+  bannerImage: "https://example.com/banner.jpg",
+  genres: ["Action", "Adventure"],
   tags: [
-    { id: 1, name: 'Adventure' },
-    { id: 2, name: 'Fantasy' },
+    { id: 1, name: "Adventure" },
+    { id: 2, name: "Fantasy" },
   ],
-  status: 'FINISHED',
-  format: 'TV',
+  status: "FINISHED",
+  format: "TV",
   episodes: 24,
   year: 2020,
-  season: 'FALL',
+  season: "FALL",
   duration: 24,
   startIn: { year: 2020, month: 10, day: 1 },
   endIn: { year: 2021, month: 3, day: 27 },
@@ -46,37 +43,110 @@ export const animeFixture: AnimeInfo = {
     decimalScore: 8.5,
   },
   popularity: 120000,
-  siteUrl: 'https://example.com/anime',
+  siteUrl: "https://example.com/anime",
   trailer: {
-    id: 'trailer1',
-    site: 'YouTube',
-    thumbnail: 'https://example.com/trailer-thumbnail.jpg',
+    id: "trailer1",
+    site: "YouTube",
+    thumbnail: "https://example.com/trailer-thumbnail.jpg",
   },
-  studios: [
-    { name: 'Studio Example' }
-  ],
+  studios: [{ name: "Studio Example" }],
   relation: [],
 };
 
-export const popularAnime: AnimePopular = {
-  id: '1',
+export const popularAnime = {
+  id: 16498,
+  idMal: 16498,
+  status: "FINISHED",
   title: {
-    userPreferred: 'My Popular Anime',
-    romaji: 'Popular Anime Romaji',
-    english: 'Popular Anime English',
-    native: '人気アニメ',
+    userPreferred: "Shingeki no Kyojin",
+    romaji: "Shingeki no Kyojin",
+    english: "Attack on Titan",
+    native: "進撃の巨人",
   },
+  genres: ["Action", "Drama", "Fantasy", "Mystery"],
+  tags: [
+    {
+      id: 82,
+      name: "Male Protagonist",
+    },
+    {
+      id: 1046,
+      name: "Suicide",
+    },
+  ],
+  trailer: {
+    id: "luYOt2-c2TI",
+    site: "youtube",
+    thumbnail: "https://i.ytimg.com/vi/luYOt2-c2TI/hqdefault.jpg",
+  },
+  description:
+    "Several hundred years ago, humans were nearly exterminated by titans. Titans are typically several stories tall, seem to have no intelligence, devour human beings and, worst of all, seem to do it for the pleasure rather than as a food source. A small percentage of humanity survived by walling themselves in a city protected by extremely high walls, even taller than the biggest of titans.<br><br>\r\nFlash forward to the present and the city has not seen a titan in over 100 years. Teenage boy Eren and his foster sister Mikasa witness something horrific as the city walls are destroyed by a colossal titan that appears out of thin air. As the smaller titans flood the city, the two kids watch in horror as their mother is eaten alive. Eren vows that he will murder every single titan and take revenge for all of mankind.<br><br>\r\n(Source: MangaHelpers) ",
+  format: "TV",
+  bannerImage:
+    "https://s4.anilist.co/file/anilistcdn/media/anime/banner/16498-8jpFCOcDmneX.jpg",
   coverImage: {
-    large: 'http://example.com/popular.jpg',
+    extraLarge:
+      "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx16498-73IhOXpJZiMF.jpg",
+    large:
+      "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-73IhOXpJZiMF.jpg",
+    medium:
+      "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx16498-73IhOXpJZiMF.jpg",
+    color: "#f1a143",
   },
-  status: 'RELEASING',
+  episodes: 25,
+  meanScore: 84,
+  duration: 24,
+  season: "SPRING",
+  seasonYear: 2013,
+  averageScore: 84,
+  nextAiringEpisode: null,
 };
 
-export const trendingAnime: AnimeTrending = {
-  id: '2',
-  title: 'My Trending Anime',
-  image: 'http://example.com/trending.jpg',
-  episodeNumber: 5,
-  episodeId: 'episode-5',
+export const trendingAnime = {
+  id: 171018,
+  idMal: 57334,
+  status: "RELEASING",
+  title: {
+    userPreferred: "Dandadan",
+    romaji: "Dandadan",
+    english: "DAN DA DAN",
+    native: "ダンダダン",
+  },
+  genres: ["Action", "Comedy", "Drama", "Romance", "Sci-Fi", "Supernatural"],
+  tags: [
+    {
+      id: 321,
+      name: "Urban Fantasy",
+    },
+  ],
+  trailer: {
+    id: "vnRmIBoyfBM",
+    site: "youtube",
+    thumbnail: "https://i.ytimg.com/vi/vnRmIBoyfBM/hqdefault.jpg",
+  },
+  description:
+    "This is a story about Momo, a high school girl who comes from a family of spirit mediums, and her classmate Okarun, an occult fanatic. After Momo rescues Okarun from being bullied, they begin talking. However, an argument ensues between them since Momo believes in ghosts but denies aliens exist, and Okarun believes in aliens but denies ghosts exist.\n<br><br>\nTo prove to each other what they believe in is real, Momo goes to an abandoned hospital where a UFO has been spotted and Okarun goes to a tunnel rumored to be haunted. To their surprise, they each encounter overwhelming paranormal activities that transcend comprehension. Amid these predicaments, Momo awakens her hidden power and Okarun gains the power of a curse to overcome these new dangers! Their fateful love begins as well!?\n<br><br>\nThe story of the occult battle and adolescence starts!<br><br>\n(Source: Crunchyroll) <br><br>\n\n<i>Notes: <br>\n- Worldwide premiere of Episode 1 before the Japanese television premiere occurred at Anime Expo July 6, 2024. <br>\n- Episodes 1-3 titled as <b>DAN DA DAN: FIRST ENCOUNTER</b> was pre-screened in advance in theaters on August 31, 2024 in Asia, September 7, 2024 in Europe and September 13, 2024 in North America. The regular TV broadcast began October 4, 2024.</i>",
+  format: "TV",
+  bannerImage:
+    "https://s4.anilist.co/file/anilistcdn/media/anime/banner/171018-SpwPNAduszXl.jpg",
+  coverImage: {
+    extraLarge:
+      "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx171018-2ldCj6QywuOa.jpg",
+    large:
+      "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx171018-2ldCj6QywuOa.jpg",
+    medium:
+      "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx171018-2ldCj6QywuOa.jpg",
+    color: "#e47850",
+  },
+  episodes: 12,
+  meanScore: 84,
+  duration: 24,
+  season: "FALL",
+  seasonYear: 2024,
+  averageScore: 84,
+  nextAiringEpisode: {
+    airingAt: 1730993160,
+    timeUntilAiring: 437497,
+    episode: 6,
+  },
 };
-
--- test/fixture/info/movie.fixture.ts
+++ test/fixture/info/movie.fixture.ts
@@ -1,52 +1,59 @@
-import { MovieInfo } from "@/types/movies.type";
-
-export const movieFixture: MovieInfo = {
+export const movieFixture = {
   adult: false,
-  backdrop_path: 'https://example.com/backdrop.jpg',
+  backdrop_path:
+    "https://wsrv.nl/?url=image.tmdb.org/t/p/original/zfbjgQE1uSd9wiPTX4VzsLi0rGG.jpg",
+  belongs_to_collection: null,
+  budget: 25000000,
   genres: [
-    { id: 18, name: 'Drama' },
-    { id: 28, name: 'Action' },
+    {
+      id: 18,
+      name: "Drama",
+    },
+    {
+      id: 80,
+      name: "Crime",
+    },
   ],
-  homepage: 'https://example.com/movie-homepage',
-  id: 2,
-  original_language: 'en',
-  overview: 'Movie description overview goes here.',
-  popularity: 5000,
-  poster_path: 'https://example.com/poster.jpg',
+  homepage: "",
+  id: 278,
+  imdb_id: "tt0111161",
+  origin_country: ["US"],
+  original_language: "en",
+  original_title: "The Shawshank Redemption",
+  overview:
+    "Imprisoned in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.",
+  popularity: 233.532,
+  poster_path:
+    "https://wsrv.nl/?url=image.tmdb.org/t/p/original/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg",
   production_companies: [
     {
-      id: 100,
-      logo_path: 'https://example.com/company-logo.png',
-      name: 'Production Company',
-      origin_country: 'US',
+      id: 97,
+      logo_path:
+        "https://wsrv.nl/?url=image.tmdb.org/t/p/original/7znWcbDd4PcJzJUlJxYqAlPPykp.png",
+      name: "Castle Rock Entertainment",
+      origin_country: "US",
     },
   ],
   production_countries: [
     {
-      iso_3166_1: 'US',
-      name: 'United States of America',
+      iso_3166_1: "US",
+      name: "United States of America",
     },
   ],
+  release_date: "1994-09-23",
+  revenue: 28341469,
+  runtime: 142,
   spoken_languages: [
     {
-      english_name: 'English',
-      iso_639_1: 'en',
-      name: 'English',
+      english_name: "English",
+      iso_639_1: "en",
+      name: "English",
     },
   ],
-  status: 'Released',
-  tagline: 'An exciting movie tagline!',
-  vote_average: 7.8,
-  vote_count: 1200,
-  genre_names: ['Drama', 'Action'],
-
-  // Movie specific fields
-  original_title: 'Original Movie Title',
-  release_date: '2021-12-01',
-  revenue: 100000000,
-  runtime: 120,
-  title: 'Movie Title',
+  status: "Released",
+  tagline: "Fear can hold you prisoner. Hope can set you free.",
+  title: "The Shawshank Redemption",
   video: false,
+  vote_average: 8.7,
+  vote_count: 27041,
 };
-
-
