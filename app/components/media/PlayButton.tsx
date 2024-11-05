6bf47f049b0ce21fa47808018664da55a0602740
alfaruqii <bumbblechover@gmail.com>
2024-11-05 16:42:53 +0700

    feat(component): update PlayButton(code formatting)

--- app/components/media/PlayButton.tsx
+++ app/components/media/PlayButton.tsx
@@ -2,12 +2,16 @@ function PlayButton() {
   return (
     <>
       <div className="absolute inset-0 flex items-center justify-center">
-        <svg className="w-20 h-20 text-white opacity-80" fill="currentColor" viewBox="0 0 20 20">
+        <svg
+          className="w-20 h-20 text-white opacity-80"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
           <path d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z" />
         </svg>
       </div>
     </>
-  )
+  );
 }
 
-export default PlayButton
+export default PlayButton;
