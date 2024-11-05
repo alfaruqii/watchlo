bf1249f9969dda103847d70a2ef9c4aebed6e815
alfaruqii <bumbblechover@gmail.com>
2024-11-05 16:52:08 +0700

    feat(component): update EpisodesComponent(desc: doesn't need to refetch the whole episode if change episode)

--- app/components/watch/episodes/EpisodesComponent.tsx
+++ app/components/watch/episodes/EpisodesComponent.tsx
@@ -1,38 +1,54 @@
-import { AnimeDetails, AnimeEpisode } from "@/types/anime.type"
-import EpisodeComponent from "./EpisodeComponent"
+import { AnimeDetails, AnimeEpisode } from "@/types/anime.type";
 import { SeriesEpisode, TV } from "@/types/movies.type";
 import { isAnimeDetails } from "@/utils/mediaTypeChecker";
+import EpisodeComponent from "./EpisodeComponent";
 
 interface Item {
   item: AnimeDetails | TV;
-  ep: number;
-  season?: number;
+  ep: string;
+  season?: string;
   id?: string;
+  isDub?: string;
+  handleEpisodeChange?: (ep: string) => void;
 }
 
-function EpisodesComponent(data: Item) {
+function EpisodesComponent({
+  item,
+  ep,
+  season,
+  id,
+  isDub,
+  handleEpisodeChange,
+}: Item) {
+  const isAnime = isAnimeDetails(item);
+
+  const episodes = isAnime
+    ? (item as AnimeDetails).episodes
+    : (item as TV).episodes;
+
   return (
-    <>
-      <div className="flex flex-col pt-1.5 lg:max-w-[50%]">
-        {
-          isAnimeDetails(data.item) ?
-            <p className="pb-1">
-              Now Playing Episode {data.ep}
-            </p> :
-            <p className="pb-1">
-              Now Playing (Season {data.season} - Episode {data.ep})
-            </p>
-        }
-        <div className="scrollbar-thumb-rounded-full scrollbar-track-rounded-full flex max-h-96 flex-wrap gap-2 overflow-y-scroll scrollbar scrollbar-track-gray-300 scrollbar-thumb-gray-800 2xl:max-h-[30rem] ">
-          {
-            isAnimeDetails(data.item) ?
-              data.item.episodes.map((i: AnimeEpisode) => <EpisodeComponent key={i.id} id={i.id} titleAndEpisode={data.item.id as string} episodeNumber={i.number} epQuery={data.ep} />) :
-              data.item.episodes.map((i: SeriesEpisode) => <EpisodeComponent key={i.id} id={String(data.id)} titleAndEpisode={data.item.id as string} episodeNumber={i.episode_number} epQuery={data.ep} isSeries season={i.season_number} ep={i.episode_number} />)
-          }
-        </div>
+    <div className="flex flex-col pt-1.5 col-span-2">
+      <p className="pb-1">
+        {isAnime
+          ? `Now Playing Episode ${ep}`
+          : `Now Playing (Season ${season} - Episode ${ep})`}
+      </p>
+      <div className="scrollbar-thumb-rounded-full scrollbar-track-rounded-full grid grid-cols-4 sm:grid-cols-6 gap-2 max-h-96 overflow-y-auto scrollbar scrollbar-track-gray-300 scrollbar-thumb-gray-800">
+        {episodes.map((episode: AnimeEpisode | SeriesEpisode) => (
+          <EpisodeComponent
+            key={episode.id}
+            isDub={isDub}
+            episode={episode}
+            isAnime={isAnime}
+            id={String(id)}
+            season={season}
+            ep={ep}
+            handleEpisodeChange={handleEpisodeChange}
+          />
+        ))}
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
 export default EpisodesComponent;
