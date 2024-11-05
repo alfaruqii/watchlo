fc7767b18761147b4c0dc62eefe62a98b0dac588
alfaruqii <bumbblechover@gmail.com>
2024-11-05 16:36:08 +0700

    feat(component): update RelationComponent(code formatting)

--- app/components/detail/relation/RelationComponent.tsx
+++ app/components/detail/relation/RelationComponent.tsx
@@ -1,19 +1,26 @@
-import { AnimeContainerCard } from "@/components/card/animecard/AnimeContainerCard";
-import { Relation } from "@/types/anime.type";
+import AnimeContainerCard from "@/components/card/animecard/AnimeContainerCard";
+import { RelationOrRecommendation } from "@/types/anime.type";
 
-function RelationComponent({ relation }: { relation: Relation[] }) {
-  const filteredRelation = relation.filter((i) => (i.format === "TV" || i.format === "MOVIE" || i.format === "SPECIAL"));
+function RelationComponent({
+  relation,
+}: {
+  relation: RelationOrRecommendation[];
+}) {
+  const filteredRelation = relation.filter(
+    (i) => i.format === "TV" || i.format === "MOVIE" || i.format === "SPECIAL"
+  );
   return (
     <>
       <div className="pt-4">
-        {
-          (filteredRelation.length > 0) && (
-            <AnimeContainerCard animes={filteredRelation} containerTitle="Relation 🧩" />
-          )
-        }
+        {filteredRelation.length > 0 && (
+          <AnimeContainerCard
+            animes={filteredRelation}
+            containerTitle="Relation 🧩"
+          />
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default RelationComponent
+export default RelationComponent;
