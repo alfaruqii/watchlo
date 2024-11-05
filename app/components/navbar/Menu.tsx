1421c71ed64aee5542abec4be1432a1045c043ca
alfaruqii <bumbblechover@gmail.com>
2024-11-05 16:43:30 +0700

    feat(component): update Menu(from local to react-icons)

--- app/components/navbar/Menu.tsx
+++ app/components/navbar/Menu.tsx
@@ -1,23 +1,33 @@
 import { useThemeStore } from "@/store/themeStore";
 import Link from "next/link";
-import DocsIcon from "../icons/DocsIcon";
-import AnimeIcon from "../icons/AnimeIcon";
-import MoviesIcon from "../icons/MoviesIcon";
-import GithubIcon from "../icons/GithubIcon";
+import { GiFilmSpool } from "react-icons/gi";
+import { GiShintoShrine } from "react-icons/gi";
+import { GiNotebook } from "react-icons/gi";
+import { FaSquareGithub } from "react-icons/fa6";
 import { motion, AnimatePresence } from "framer-motion";
 
-function Menu({ isToggled = false }) {
+function Menu({
+  isToggled = false,
+  closeMenu,
+}: {
+  isToggled: boolean;
+  closeMenu: () => void;
+}) {
   const { theme } = useThemeStore();
 
   const menuItems = [
-    { href: "/", icon: <MoviesIcon />, label: "Movies" },
-    { href: "/anime", icon: <AnimeIcon />, label: "Anime (No ads)" },
-    { href: "/docs", icon: <DocsIcon />, label: "Docs" },
+    { href: "/", icon: <GiFilmSpool size={24} />, label: "Movies" },
+    {
+      href: "/anime",
+      icon: <GiShintoShrine size={24} />,
+      label: "Anime (No ads)",
+    },
+    { href: "/docs", icon: <GiNotebook size={24} />, label: "Docs" },
     {
       href: "https://github.com/alfaruqii/watchlo",
-      icon: <GithubIcon />,
+      icon: <FaSquareGithub size={24} />,
       label: "Github",
-      external: true
+      external: true,
     },
   ];
 
@@ -32,7 +42,11 @@ function Menu({ isToggled = false }) {
           className={`
             fixed left-0 right-0 top-12 z-50 w-full overflow-hidden
             border-b pb-2
-            ${theme === "garden" ? "bg-base-100 border-gray-700/20" : "bg-black border-gray-300/20"}
+            ${
+              theme === "garden"
+                ? "bg-base-100 border-gray-700/20"
+                : "bg-black border-gray-300/20"
+            }
             drop-shadow-xl 
           `}
         >
@@ -43,17 +57,19 @@ function Menu({ isToggled = false }) {
               animate={{ opacity: 1, y: 0, x: 0 }}
               transition={{ delay: index * 0.08 }}
             >
-              <Link
-                href={item.href}
-                {...(item.external ? { target: "_blank" } : {})}
-                className="flex h-full w-full items-center gap-1.5 px-4 py-3 
+              <button type="button" onClick={() => closeMenu()}>
+                <Link
+                  href={item.href}
+                  {...(item.external ? { target: "_blank" } : {})}
+                  className="flex h-full w-full items-center gap-1.5 px-4 py-3 
                            transition duration-100"
-              >
-                <span className="flex items-center justify-center">
-                  {item.icon}
-                </span>
-                {item.label}
-              </Link>
+                >
+                  <span className="flex items-center justify-center">
+                    {item.icon}
+                  </span>
+                  {item.label}
+                </Link>
+              </button>
             </motion.li>
           ))}
         </motion.ul>
