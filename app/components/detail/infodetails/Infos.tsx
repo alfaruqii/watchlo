0e3aef56caf261d56eb3f89f6e62ea3dbd03640a
alfaruqii <bumbblechover@gmail.com>
2024-11-05 16:34:01 +0700

    feat(component): update Infos(handle not wanted value)

--- app/components/detail/infodetails/Infos.tsx
+++ app/components/detail/infodetails/Infos.tsx
@@ -1,22 +1,22 @@
 type Infos = {
   topic: string;
-  value: string | number;
+  value?: string | number;
   customTheme?: string;
-}
+};
 
 function Infos({ topic, value, customTheme }: Infos) {
   return (
     <>
       <div className="flex w-full justify-between">
-        <p>
-          {topic}
-        </p>
-        <p className={`${customTheme} capitalize`}>
+        <p className="font-semibold">{topic}</p>
+        <p
+          className={`${customTheme} max-w-64 text-right line-clamp-1 capitalize sm:max-w-full`}
+        >
           {value ?? "unknown"}
         </p>
       </div>
     </>
-  )
+  );
 }
 
-export default Infos
+export default Infos;
