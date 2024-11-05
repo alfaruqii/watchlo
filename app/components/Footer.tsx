b435d7819d376abe6bb51f964fad77234fa4ce0a
alfaruqii <bumbblechover@gmail.com>
2024-11-05 16:52:37 +0700

    feat(component): update Footer(add creator & code formatting)

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,22 +1,27 @@
 "use client";
-import { useThemeStore } from "@/store/themeStore"
+import { useThemeStore } from "@/store/themeStore";
 
 function Footer() {
   const { theme } = useThemeStore();
   return (
     <>
-      <div className={`${theme === "garden" ? "border-gray-700/20" : "border-gray-300/20"} flex flex-col items-center border-t p-4 text-xs`}>
+      <div
+        data-testid="footer-container"
+        className={`${
+          theme === "garden" ? "border-gray-700/20" : "border-gray-300/20"
+        } flex flex-col items-center border-t p-4 text-xs`}
+      >
         <div>
           <p>
-            Disclaimer | Please use this site wisely, dont use it to search some inappropriate film, because the purpose of this project is not for that, grow up.
+            Disclaimer | Please use this site wisely, dont use it to search some
+            inappropriate film, because the purpose of this project is not for
+            that, grow up.
           </p>
-          <p className="mt-2">
-            Copyright © 2024 Watchlo | Made by faruqi.
-          </p>
+          <p className="mt-2">Copyright © 2024 Watchlo | Made by faruqi.</p>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
