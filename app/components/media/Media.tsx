e09141517fa0d33f93dfca2c8ee5fd245f83941f
alfaruqii <bumbblechover@gmail.com>
2024-11-05 16:42:27 +0700

    feat(component): update Media(add gesture[+10sec] handle recent anime)

--- app/components/media/Media.tsx
+++ app/components/media/Media.tsx
@@ -1,70 +1,201 @@
-"use client";
-import { StreamInfo } from '@/types/anime.type';
-import { MediaPlayer, MediaPlayerInstance, MediaProvider, Poster } from '@vidstack/react';
-import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
-import { useRef, useState, useEffect } from 'react';
-import Quality from '../quality/Quality';
+import { StreamInfo } from "@/types/anime.type";
+import {
+  Gesture,
+  MediaPlayer,
+  MediaPlayerInstance,
+  MediaProvider,
+  Poster,
+} from "@vidstack/react";
+import {
+  defaultLayoutIcons,
+  DefaultVideoLayout,
+} from "@vidstack/react/player/layouts/default";
+import Hls, { ErrorData, Events } from "hls.js";
+import { useRef, useState, useEffect } from "react";
+import Quality from "../quality/Quality";
+import useSWR from "swr";
+import SkeletonMediaPlayer from "../skeleton/SkeletonMediaPlayer";
 
-interface MediaProps extends StreamInfo {
+interface MediaProps {
   poster: string;
   title: string;
+  episodeId: string;
+  ep: string;
 }
 
-export const Media = ({ title, poster, sources }: MediaProps) => {
+const fetchAnimeStream = async (episodeId: string | null) => {
+  if (!episodeId) return null;
+  const res = await fetch(`/api/animestream?query=${episodeId}`);
+  return res.json();
+};
+
+const Media = ({ title, poster, episodeId, ep }: MediaProps) => {
   const playerRef = useRef<MediaPlayerInstance>(null);
-  const [vidSrc, setVidSrc] = useState<string>(
-    (sources.find(source => source.quality === "default") ?? sources[0])?.url || ''
-  );
+  const hlsRef = useRef<Hls | null>(null);
+  const [vidSrc, setVidSrc] = useState<string>("");
   const [currentTime, setCurrentTime] = useState<number>(0);
-  const [, setIsReady] = useState<boolean>(false);
+  const previousEp = useRef<string | null>("");
+
+  const {
+    data: streamInfo,
+    error: errorStream,
+    isLoading,
+  } = useSWR<StreamInfo>(() => episodeId || null, fetchAnimeStream);
 
   useEffect(() => {
-    const player = playerRef.current;
-    if (player) {
-      const onCanPlay = () => {
-        setIsReady(true);
-        player.currentTime = currentTime;
-        if (player.paused) {
-          player.play().catch(error => console.error('Error playing video:', error));
-        }
-      };
-
-      player.addEventListener('can-play', onCanPlay);
-
-      // Clean up the event listener
-      return () => {
-        player.removeEventListener('can-play', onCanPlay);
-      };
+    if (previousEp.current !== null && previousEp.current !== ep) {
+      setCurrentTime(0);
     }
+    previousEp.current = ep;
+  }, [ep]);
+
+  useEffect(() => {
+    if (streamInfo?.sources && streamInfo?.sources.length > 0) {
+      const defaultSource =
+        streamInfo?.sources.find((source) => source.quality === "default") ??
+        streamInfo?.sources[0];
+      setVidSrc(defaultSource.url);
+    }
+  }, [episodeId, streamInfo?.sources]);
+
+  useEffect(() => {
+    if (!vidSrc) return;
+
+    const player = playerRef.current;
+    if (!player) return;
+
+    const initializeHls = () => {
+      if (!Hls.isSupported() || !vidSrc.includes(".m3u8")) return;
+
+      const hls = new Hls({
+        maxBufferSize: 30 * 1000 * 1000,
+        enableWorker: true,
+        startLevel: -1,
+        lowLatencyMode: true,
+        backBufferLength: 90,
+        abrEwmaDefaultEstimate: 1000000,
+        abrBandWidthFactor: 0.95,
+        abrBandWidthUpFactor: 0.7,
+        fragLoadingTimeOut: 20000,
+        manifestLoadingTimeOut: 20000,
+      });
+
+      hls.loadSource(vidSrc);
+
+      const checkProviderAndInitialize = () => {
+        if (!player.provider) {
+          requestAnimationFrame(checkProviderAndInitialize);
+          return;
+        }
+
+        const mediaElement =
+          player.provider?.type === "video" || player.provider?.type === "hls"
+            ? player.provider?.video
+            : null;
+
+        if (!mediaElement) {
+          console.error("No media element found");
+          return;
+        }
+
+        hls.attachMedia(mediaElement);
+
+        hls.on(Events.MANIFEST_PARSED, () => {
+          player.currentTime = currentTime;
+          player.play().catch((playError: Error) => {
+            console.error("Error playing video:", playError);
+          });
+        });
+
+        hls.on(Hls.Events.ERROR, (_event: Events, data: ErrorData) => {
+          if (data.fatal) {
+            switch (data.type) {
+              case Hls.ErrorTypes.NETWORK_ERROR:
+                hls.startLoad();
+                break;
+              case Hls.ErrorTypes.MEDIA_ERROR:
+                hls.recoverMediaError();
+                break;
+              default:
+                initializeHls();
+                break;
+            }
+          }
+        });
+
+        hlsRef.current = hls;
+      };
+
+      checkProviderAndInitialize();
+    };
+
+    initializeHls();
+
+    return () => {
+      if (hlsRef.current) {
+        hlsRef.current.destroy();
+        hlsRef.current = null;
+      }
+    };
   }, [vidSrc, currentTime]);
 
   const handleQualityChange = (newSrc: string): void => {
     const player = playerRef.current;
     if (player) {
       setCurrentTime(player.currentTime);
-      setIsReady(false);
       setVidSrc(newSrc);
     }
   };
 
+  if (errorStream) {
+    return (
+      <div className="col-span-3">There is an error in our application :[</div>
+    );
+  }
+
+  if (episodeId && isLoading) {
+    return <SkeletonMediaPlayer />;
+  }
+
+  if (!episodeId || !streamInfo?.sources || streamInfo?.sources.length === 0) {
+    return <div className="col-span-3">No video sources available</div>;
+  }
+
   return (
-    <>
-      <div className="lg:w-1/2">
-        <h1 className="text-xl pb-1 font-black drop-shadow-lg text-center sm:text-left line-clamp-1">
-          {title}
-        </h1>
-        <MediaPlayer className="" ref={playerRef} title={title} src={vidSrc}>
-          <MediaProvider>
-            <Poster
-              className="vds-poster"
-              src={`${poster}`}
-              alt={`${title}`}
-            />
-          </MediaProvider>
-          <DefaultVideoLayout thumbnails={poster} icons={defaultLayoutIcons} />
-        </MediaPlayer>
-        <Quality handleQualityChange={handleQualityChange} sources={sources} />
-      </div>
-    </>
+    <div className="lg:col-span-3">
+      <h1 className="line-clamp-1 pb-1 text-center text-xl font-black drop-shadow-lg sm:text-left">
+        {title}
+      </h1>
+      <MediaPlayer
+        autoPlay
+        ref={playerRef}
+        title={title}
+        src={vidSrc}
+        load="eager"
+        posterLoad="eager"
+      >
+        <MediaProvider>
+          <Poster className="vds-poster" src={poster} alt={title} />
+        </MediaProvider>
+        <DefaultVideoLayout icons={defaultLayoutIcons} />
+        <Gesture
+          className="absolute inset-0 z-10 block h-full w-1/5"
+          event="dblpointerup"
+          action="seek:-10"
+        />
+        <Gesture
+          className="absolute inset-0 z-10 block h-full w-1/5"
+          event="dblpointerup"
+          action="seek:10"
+        />
+        <DefaultVideoLayout thumbnails={poster} icons={defaultLayoutIcons} />
+      </MediaPlayer>
+      <Quality
+        handleQualityChange={handleQualityChange}
+        sources={streamInfo?.sources}
+      />
+    </div>
   );
 };
+
+export default Media;
