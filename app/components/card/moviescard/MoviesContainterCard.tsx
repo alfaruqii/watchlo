af3dd77a1286440e55fc3723fd3c39f088526eee
alfaruqii <bumbblechover@gmail.com>
2024-11-14 20:44:22 +0700

    feat(component): update component(code formatting)

--- app/components/card/moviescard/MoviesContainterCard.tsx
+++ app/components/card/moviescard/MoviesContainterCard.tsx
@@ -1,32 +1,42 @@
 "use client";
 
-import { useThemeStore } from '@/store/themeStore'
-import { MoviesCard } from './MoviesCard'
-import { MovieInfo, TVInfo } from '@/types/movies.type';
+import { useThemeStore } from "@/store/themeStore";
+import { MoviesCard } from "./MoviesCard";
+import { MovieInfo, TVInfo } from "@/types/movies.type";
 
 interface MoviesContainerProps {
   movies: MovieInfo[] | TVInfo[];
-  containerTitle: string
+  containerTitle: string;
   isDetail?: boolean;
   season?: number;
   ep?: number;
 }
 
-export const MoviesContainerCard = ({ movies, containerTitle, isDetail = true, season, ep }: MoviesContainerProps) => {
+export const MoviesContainerCard = ({
+  movies,
+  containerTitle,
+  isDetail = true,
+  season,
+  ep,
+}: MoviesContainerProps) => {
   const { theme } = useThemeStore();
 
   return (
     <div className="overflow-hidden sm:p-4">
-      {
-        containerTitle &&
-        <p className={`mb-4 pb-2 font-bold border-b ${theme === "garden" ? "border-black" : "border-gray-200"}`}>
+      {containerTitle && (
+        <p
+          className={`${
+            theme === "garden" ? "border-black" : "border-gray-200"
+          } mb-4 border-b pb-2
+          font-bold`}
+        >
           {containerTitle}
         </p>
-      }
-      <div className="flex embla__container gap-4 relative w-full overflow-x-scroll scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-gray-800 scrollbar-track-gray-300 pb-2">
-        {movies.map((item) => (
+      )}
+      <div className="embla__container scrollbar-thumb-rounded-full scrollbar-track-rounded-full relative flex w-full gap-4 overflow-x-scroll pb-2 scrollbar-track-gray-300 scrollbar-thumb-gray-800">
+        {movies.map((item, i) => (
           <MoviesCard
-            key={item.id}
+            key={i}
             movie={item}
             isDetail={isDetail}
             season={season}
@@ -35,5 +45,5 @@ export const MoviesContainerCard = ({ movies, containerTitle, isDetail = true, s
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
